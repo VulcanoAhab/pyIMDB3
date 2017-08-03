e7c2b657392a0cd574a87665cb912d65014b895a
VulcanoAhab <vulcano.ahab@gmail.com>
2017-08-02 23:43:20 -0300

    Starting repo

--- titleMiner.py
+++ titleMiner.py
@@ -5,12 +5,21 @@ class Miner():
     """
 
     @staticmethod
-    def _testEls(listEl, targetName):
+    def _existsEl(listEls, targetName):
         """
         """
-        size=len(listEl)
+        size=len(listEls)
         if not size:print("[-] Could not find: {}".format(targetName))
         return size
+
+    @staticmethod
+    def _getEl(listEl, targetName):
+        """
+        """
+        if listEl:return listEl[0]
+        print("[-] Could not find: {}".format(targetName))
+        return None
+
 
     def __init__(self, engine=html):
         """
@@ -27,15 +36,24 @@ class Miner():
         """
         """
         titleContainer=self._obj.xpath(".//title/text()")
-        if not Miner._testEls(titleContainer, "page title"):return
-        return titleContainer[0]
+        return Miner._getEl(titleContainer, "page title")
 
     def mineActors(self):
         """
         """
         castElement=self._obj.xpath(".//table[@class='cast_list']")
-        if not Miner._testEls(castElement,"cast table"):return
+        if not Miner._existsEl(castElement,"cast table"):return
         castRows=castElement[0].xpath(".//tr[@class='even' or @class='odd']")
-        if not Miner._testEls(castRows,"cast rows"):return
+        if not Miner._existsEl(castRows,"cast rows"):return
+        castList=[]
         for castRow in castRows:
-            tdImage=castRow.xpath(".//td[@class='primary_image']")
+            tdPersonEl=castRow.xpath(".//td[@itemprop='actor']")
+            tdPersonProp=Miner._getEl(tdPersonEl, "actor_element")
+            castPersonIdEl=tdPersonProp.xpath(".//a/@href")
+            castPersonId=Miner._getEl(castPersonIdEl, "actor_id")
+            castPersonNameEl=tdPersonProp.xpath(".//span/text()")
+            castPersonName=Miner._getEl(castPersonNameEl, "actor_name")
+            castList.append({
+                "actorId":parsePersonId(castPersonId),
+                "actorName":parsePersonName(castPersonName)
+            })
